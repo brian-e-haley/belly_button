718e9be929e895cb257a7ca106fef33f630efd79
TotalEntropy <26179509+TotalEntropy@users.noreply.github.com>
2019-05-25 12:31:21 -0400

    Pie chart and bug fixing for sample selector

--- static/js/app.js
+++ static/js/app.js
@@ -2,16 +2,17 @@ function buildMetametadata(sample) {
 
   // @TODO: Complete the following function that builds the metametadata panelMetametadata
 
-  var metadataURL = "/metametadata/" + sample;
-  var panelMetadata = d3.select("#sample-metametadata");
+  var metadataURL = "/metadata/" + sample;
+  var panelMetadata = d3.select("#sample-metadata");
   panelMetadata.html("");
 
   d3.json(metadataURL).then(function (data) {
-    console.log(data);
-    Object.defineProperties(data).forEach(([key, value]) => {
+    console.log("data selected: ", data);
+    Object.entries(data).forEach(([key, value]) => {
       panelMetadata.append("h6").text(`${key}: ${value}`);
     });
   });
+
     // BONUS: Build the Gauge Chart
     // buildGauge(metadata.WFREQ);
 };
@@ -23,7 +24,7 @@ function buildCharts(sample) {
   // @TODO: Use `d3.json` to fetch the sample metadata for the plots
 
   d3.json(chartURL).then(function (data) {
-    var trace1 = {
+    var trace = {
       x: data.otu_ids,
       y: data.sample_values,
       mode: 'markers',
@@ -31,31 +32,43 @@ function buildCharts(sample) {
       marker: {
         color: data.otu_ids,
         size: data.sample_values,
-
         colorscale: "Earth"
       }
     };
 
     // @TODO: Build a Bubble Chart using the sample metadata
 
-    var trace1 = [trace1];
-    var layout = {
+    var traceBubble = [trace];
+    var layoutBubble = {
       showlegend: false,
       height: 600,
       width: 1500
     };
-
-    Plotly.newPlot('bubble', trace1, layout);
+    
+    Plotly.newPlot('bubble', traceBubble, layoutBubble);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+    var dataPie = [{
+      values: data.sample_values.slice(0, 10),
+      labels: data.otu_ids.slice(0, 10),
+      hovertext: data.otu_labels.slice(0 ,10),
+      type: 'pie'
+    }];
+
+    var layoutPie = {
+      showlegend: true
+    };
+
+    Plotly.newPlot('pie', dataPie, layoutPie)
   });
-}
+};
 
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selmetadataset");
+  var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
